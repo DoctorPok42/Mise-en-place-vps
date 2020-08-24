21dc285cc704d953a523be9362cf0aeee418d4fa
DoctorPok42 <58393254+DoctorPok42@users.noreply.github.com>
2020-08-24 15:29:36 +0200

    Initial commit

---
+++ [4] Commande node.js
@@ -0,0 +1,6 @@
+apt-get install sudo
+
+1 - sudo apt-get install curl
+2 - curl -sL https://deb.nodesource.com/setup_10.x | bash - ou curl -sL https://deb.nodesource.com/setup_12.x | bash -
+3 - sudo apt-get install -y nodejs
+4 - sudo apt-get install -y build-essential
